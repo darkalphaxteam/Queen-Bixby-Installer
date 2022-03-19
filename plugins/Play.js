53f6ccfdf6756cb9f10a4ee9734ee71bdad8c9a5
Ripper Sir <92292759+RIPPER-SER@users.noreply.github.com>
2022-03-19 21:25:55 +0530

    Update Play.js

--- plugins/Play.js
+++ plugins/Play.js
@@ -1,7 +1,7 @@
 /* Copyright (C) 2022 RIPPER-SER.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-https://github.com/RIPPER-SER/bixbymowl
+https://github.com/RIPPER-SER/Bixby-Mowl
 */
 
 

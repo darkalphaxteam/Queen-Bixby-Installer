7f2f10f4a8fc6db86a2a5c1673825b15f9353a6f
Ripper Sir <92292759+RIPPER-SER@users.noreply.github.com>
2022-03-22 16:28:05 +0530

    update black.js

--- plugins/black.js
+++ plugins/black.js
@@ -4,7 +4,7 @@ you may not use this file except in compliance with the License.
 https://github.com/RIPPER-SER/bixbymowl
 */
 
-const Ktb = require('../events');
+const Bixby = require('../events');
 const {MessageType, Mimetype} = require('@adiwajshing/baileys');
 const fs = require('fs');
 const ffmpeg = require('fluent-ffmpeg');
@@ -12,7 +12,7 @@ const {execFile} = require('child_process');
 const cwebp = require('cwebp-bin');
 const Config = require('../config');
 
-    Ktb.addCommand({pattern: 'sendi$', fromMe: true, desc: 'Download status from wa'}, (async (message, match) => {    
+    Bixby.addCommand({pattern: 'sendi$', fromMe: true, desc: 'Download status from wa'}, (async (message, match) => {    
 
         if (message.reply_message === false) return await message.client.sendMessage(message.jid,'reply to a status', MessageType.text);
         var downloading = await message.client.sendMessage(message.jid,'*Downloading Image Status* \n\n ```If it is video status try cmd [.sndv]```',MessageType.text);
@@ -28,7 +28,7 @@ const Config = require('../config');
             ffmpeg(location)
                 .save('status.png')
                 .on('end', async () => {
-                    await message.client.sendMessage(message.jid,fs.readFileSync('status.png'), MessageType.image ,{caption: '```WHATSAPP STATUS 🍃```'  , mimetype: Mimetype.jpg, quoted: message.data});
+                    await message.client.sendMessage(message.jid,fs.readFileSync('status.png'), MessageType.image ,{caption: '```©Bixby Mowl```'  , mimetype: Mimetype.jpg, quoted: message.data});
             });
         return 
         }
@@ -36,14 +36,14 @@ const Config = require('../config');
         ffmpeg(location)
             .save('status.mp4')
             .on('end', async () => {
-                await message.client.sendMessage(message.jid,fs.readFileSync('status.mp4'), MessageType.video , {caption: '```WHATSAPP STATUS  🍃```'  , mimetype: Mimetype.mp4, quoted: message.data});
+                await message.client.sendMessage(message.jid,fs.readFileSync('status.mp4'), MessageType.video , {caption: '```©Bixby Mowl```'  , mimetype: Mimetype.mp4, quoted: message.data});
             });
         return 
     }));
     
     
     
-   Ktb.addCommand({pattern: 'sendv$', fromMe: true, desc: 'Download status from wa'}, (async (message, match) => {    
+   Bixby.addCommand({pattern: 'sendv$', fromMe: true, desc: 'Download status from wa'}, (async (message, match) => {    
 
         if (message.reply_message === false) return await message.client.sendMessage(message.jid,'reply to a status', MessageType.text);
         var downloading = await message.client.sendMessage(message.jid,'*Downloading Video Status* \n\n ```If it is image try cmd [.sndi]```',MessageType.text);
@@ -59,7 +59,7 @@ const Config = require('../config');
             ffmpeg(location)    
             .save('output.mp4')
             .on('end', async () => {
-                await message.client.sendMessage(message.jid, fs.readFileSync('output.mp4'), MessageType.video ,{caption: '```WHATSAPP STATUS  🍃```'  , mimetype: Mimetype.mp4, quoted: message.data});
+                await message.client.sendMessage(message.jid, fs.readFileSync('output.mp4'), MessageType.video ,{caption: '```©Bixby Mowl```'  , mimetype: Mimetype.mp4, quoted: message.data});
             });
         return 
         }
@@ -67,7 +67,7 @@ const Config = require('../config');
         ffmpeg(location)    
             .save('output.mp4')
             .on('end', async () => {
-                await message.client.sendMessage(message.jid, fs.readFileSync('output.mp4'), MessageType.video, {caption: '```WHATSAPP STATUS  🍃```'  , mimetype: Mimetype.mp4, quoted: message.data});
+                await message.client.sendMessage(message.jid, fs.readFileSync('output.mp4'), MessageType.video, {caption: '```©Bixby Mowl```'  , mimetype: Mimetype.mp4, quoted: message.data});
             });
         return 
     }));

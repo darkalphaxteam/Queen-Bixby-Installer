ed427cc99b3f75cd1e2930845051d31ede647108
Ripper Sir <92292759+RIPPER-SER@users.noreply.github.com>
2022-03-22 17:46:36 +0530

    update attp.js

--- plugins/attp.js
+++ plugins/attp.js
@@ -6,7 +6,7 @@ https://github.com/RIPPER-SER/bixbymowl
 
 
 
-const Asena = require('../events');
+const Bixby = require('../events');
 const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
 const fs = require('fs');
 const axios = require('axios');
@@ -28,44 +28,44 @@ const Lang = Language.getString('ttp');
 
 
 if (Config.WORKTYPE == 'private') {
-    Asena.addCommand({ pattern: 'ttp ?(.*)', fromMe: true, desc: Lang.TTP_DESC }, (async (message, match) => {
+    Bixby.addCommand({ pattern: 'ttp ?(.*)', fromMe: true, desc: Lang.TTP_DESC }, (async (message, match) => {
         if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORD, MessageType.text);
         var uri = encodeURI(match[1])
         var ttinullimage = await axios.get('https://api.xteam.xyz/ttp?file&text=' + uri, { responseType: 'arraybuffer' })
-        await message.client.sendMessage(message.jid,Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: 'Made by publicbot' })
+        await message.client.sendMessage(message.jid,Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: 'Made By Bixby Mowl' })
     }));
-    Asena.addCommand({ pattern: 'attp ?(.*)', fromMe: true, desc: Lang.ATTP_DESC }, (async (message, match) => {
+    Bixby.addCommand({ pattern: 'attp ?(.*)', fromMe: true, desc: Lang.ATTP_DESC }, (async (message, match) => {
         if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORD, MessageType.text);
         var uri = encodeURI(match[1])
         var ttinullimage = await axios.get('https://api.xteam.xyz/attp?file&text=' + uri, { responseType: 'arraybuffer' })
         await message.client.sendMessage(message.jid,Buffer.from(ttinullimage.data), MessageType.sticker, { mimetype: Mimetype.webp })
     }));
-Asena.addCommand({ pattern: 'bob ?(.*)', fromMe: true, desc: DESC_BOB }, (async (message, match) => {
+Bixby.addCommand({ pattern: 'bob ?(.*)', fromMe: true, desc: DESC_BOB }, (async (message, match) => {
 if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORD, MessageType.text);
 var uria = encodeURI(match[1])
 var ttinullimageh = await axios.get(`https://zekais-api.herokuapp.com/sbburn?text=${uria}&apikey=${Lol}`, { responseType: 'arraybuffer' })
 await message.client.sendMessage(message.jid,Buffer.from(ttinullimageh.data), MessageType.image)
 }));
- Asena.addCommand({ pattern: 'gura ?(.*)', fromMe: true, desc: DESC_GURA }, (async (message, match) => {
+ Bixby.addCommand({ pattern: 'gura ?(.*)', fromMe: true, desc: DESC_GURA }, (async (message, match) => {
 if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORD, MessageType.text);
 var uria = encodeURI(match[1])
 var ttinullimageh = await axios.get(`https://hardianto-chan.herokuapp.com/api/bot/gura?apikey=hardianto&nama=${uria}`, { responseType: 'arraybuffer' })
 await message.client.sendMessage(message.jid,Buffer.from(ttinullimageh.data), MessageType.image)
 }));
-Asena.addCommand({ pattern: 'harta ?(.*)', fromMe: true, desc: DESC_RAND }, (async (message, match) => {
+Bixby.addCommand({ pattern: 'harta ?(.*)', fromMe: true, desc: DESC_RAND }, (async (message, match) => {
 if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORD, MessageType.text);
 var uria = encodeURI(match[1])
 var ttinullimageh = await axios.get(`https://hardianto-chan.herokuapp.com/api/harta?query=${uria}&apikey=hardianto`, { responseType: 'arraybuffer' })
 await message.client.sendMessage(message.jid,Buffer.from(ttinullimageh.data), MessageType.image)
 }));
-Asena.addCommand({ pattern: 'rip ?(.*)', fromMe: true, desc: DESC_RIP }, (async (message, match) => {
+Bixby.addCommand({ pattern: 'rip ?(.*)', fromMe: true, desc: DESC_RIP }, (async (message, match) => {
 if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORD, MessageType.text);
 var uria = encodeURI(match[1])
 var ttinullimageh = await axios.get(`https://hardianto-chan.herokuapp.com/api/rip?image=${uria}&apikey=hardianto`, { responseType: 'arraybuffer' })
 await message.client.sendMessage(message.jid,Buffer.from(ttinullimageh.data), MessageType.image)
 }));
     
-Asena.addCommand({pattern: 'slot ?(.*)', fromMe: true, desc: 'Mini game for you'}, (async (message, match) => { 
+Bixby.addCommand({pattern: 'slot ?(.*)', fromMe: true, desc: 'Mini game for you'}, (async (message, match) => { 
         const sotoy = [
 		'💖 : 🖤 : 🤍',
 		'🖤 : 💙 : 💖',
@@ -92,21 +92,21 @@ const somtoy = sotoy[Math.floor(Math.random() * (sotoy.length))]
 await message.sendMessage(`[  🎰 | SLOTS ]\n-----------------\n💚 : ❤️ : 💝\n${somtoy}\n💚 : ❤️ : 💝\n[  🎰 | SLOTS ]`);
 }));
     
-Asena.addCommand({ pattern: 'qrcode ?(.*)', fromMe: true, desc: DESC_RAND }, (async (message, match) => {
+Bixby.addCommand({ pattern: 'qrcode ?(.*)', fromMe: true, desc: DESC_RAND }, (async (message, match) => {
 if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORD, MessageType.text);
 var uria = encodeURI(match[1])
 var ttinullimageh = await axios.get(`https://api.zeks.me/api/qrencode?apikey=apivinz&text=${uria}`, { responseType: 'arraybuffer' })
 await message.client.sendMessage(message.jid,Buffer.from(ttinullimageh.data), MessageType.image)
 }));
     
-Asena.addCommand({ pattern: 'nulis ?(.*)', fromMe: true, desc: DESC_RAND }, (async (message, match) => {
+Bixby.addCommand({ pattern: 'nulis ?(.*)', fromMe: true, desc: DESC_RAND }, (async (message, match) => {
 if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORD, MessageType.text);
 var uria = encodeURI(match[1])
 var ttinullimageh = await axios.get(`https://api.zeks.me/api/nulis?apikey=apivinz&text=${uria}`, { responseType: 'arraybuffer' })
 await message.client.sendMessage(message.jid,Buffer.from(ttinullimageh.data), MessageType.image)
 }));
     
-Asena.addCommand({ pattern: 'sand ?(.*)', fromMe: true, desc: DESC_RAND }, (async (message, match) => {
+Bixby.addCommand({ pattern: 'sand ?(.*)', fromMe: true, desc: DESC_RAND }, (async (message, match) => {
 if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORD, MessageType.text);
 var uria = encodeURI(match[1])
 var ttinullimageh = await axios.get(`https://api.zeks.me/api/sandw?apikey=apivinz&text=${uria}`, { responseType: 'arraybuffer' })
@@ -114,27 +114,27 @@ await message.client.sendMessage(message.jid,Buffer.from(ttinullimageh.data), Me
 }));
 }
 else if (Config.WORKTYPE == 'public') {
-    Asena.addCommand({ pattern: 'ttp ?(.*)', fromMe: false, desc: Lang.TTP_DESC }, (async (message, match) => {
+    Bixby.addCommand({ pattern: 'ttp ?(.*)', fromMe: false, desc: Lang.TTP_DESC }, (async (message, match) => {
         if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORD, MessageType.text);
         var uri = encodeURI(match[1])
         var ttinullimage = await axios.get('https://api.xteam.xyz/ttp?file&text=' + uri, { responseType: 'arraybuffer' })
-        await message.client.sendMessage(message.jid,Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: 'Made by publicbot' })
+        await message.client.sendMessage(message.jid,Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: 'Made By Bixby Mowl' })
     }));
-    Asena.addCommand({ pattern: 'attp ?(.*)', fromMe: false, desc: Lang.ATTP_DESC }, (async (message, match) => {
+    Bixby.addCommand({ pattern: 'attp ?(.*)', fromMe: false, desc: Lang.ATTP_DESC }, (async (message, match) => {
         if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORD, MessageType.text);
         var uri = encodeURI(match[1])
         var ttinullimage = await axios.get('https://api.xteam.xyz/attp?file&text=' + uri, { responseType: 'arraybuffer' })
         await message.client.sendMessage(message.jid,Buffer.from(ttinullimage.data), MessageType.sticker, { mimetype: Mimetype.webp })
     }));
     
-Asena.addCommand({ pattern: 'bob ?(.*)', fromMe: false, desc: DESC_BOB }, (async (message, match) => {
+Bixby.addCommand({ pattern: 'bob ?(.*)', fromMe: false, desc: DESC_BOB }, (async (message, match) => {
 if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORD, MessageType.text);
 var uria = encodeURI(match[1])
 var ttinullimageh = await axios.get(`https://zekais-api.herokuapp.com/sbburn?text=${uria}&apikey=${Lol}`, { responseType: 'arraybuffer' })
 await message.client.sendMessage(message.jid,Buffer.from(ttinullimageh.data), MessageType.image)
 }));
     
-    Asena.addCommand({pattern: 'slot ?(.*)', fromMe: false, desc: 'Mini game for you'}, (async (message, match) => { 
+    Bixby.addCommand({pattern: 'slot ?(.*)', fromMe: false, desc: 'Mini game for you'}, (async (message, match) => { 
         const sotoy = [
 		'💖 : 🖤 : 🤍',
 		'🖤 : 💙 : 💖',
@@ -161,42 +161,42 @@ const somtoy = sotoy[Math.floor(Math.random() * (sotoy.length))]
 await message.sendMessage(`[  🎰 | SLOTS ]\n-----------------\n💚 : ❤️ : 💝\n${somtoy}\n💚 : ❤️ : 💝\n[  🎰 | SLOTS ]`);
 }));
     
-Asena.addCommand({ pattern: 'gura ?(.*)', fromMe: false, desc: DESC_GURA }, (async (message, match) => {
+Bixby.addCommand({ pattern: 'gura ?(.*)', fromMe: false, desc: DESC_GURA }, (async (message, match) => {
 if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORD, MessageType.text);
 var uria = encodeURI(match[1])
 var ttinullimageh = await axios.get(`https://hardianto-chan.herokuapp.com/api/bot/gura?apikey=hardianto&nama=${uria}`, { responseType: 'arraybuffer' })
 await message.client.sendMessage(message.jid,Buffer.from(ttinullimageh.data), MessageType.image)
 }));
     
-Asena.addCommand({ pattern: 'harta ?(.*)', fromMe: false, desc: DESC_RAND }, (async (message, match) => {
+Bixby.addCommand({ pattern: 'harta ?(.*)', fromMe: false, desc: DESC_RAND }, (async (message, match) => {
 if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORD, MessageType.text);
 var uria = encodeURI(match[1])
 var ttinullimageh = await axios.get(`https://hardianto-chan.herokuapp.com/api/harta?query=${uria}&apikey=hardianto`, { responseType: 'arraybuffer' })
 await message.client.sendMessage(message.jid,Buffer.from(ttinullimageh.data), MessageType.image)
 }));
     
-Asena.addCommand({ pattern: 'rip ?(.*)', fromMe: false, desc: DESC_RIP }, (async (message, match) => {
+Bixby.addCommand({ pattern: 'rip ?(.*)', fromMe: false, desc: DESC_RIP }, (async (message, match) => {
 if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORD, MessageType.text);
 var uria = encodeURI(match[1])
 var ttinullimageh = await axios.get(`https://hardianto-chan.herokuapp.com/api/rip?image=${uria}&apikey=hardianto`, { responseType: 'arraybuffer' })
 await message.client.sendMessage(message.jid,Buffer.from(ttinullimageh.data), MessageType.image)
 }));
     
-Asena.addCommand({ pattern: 'qrcode ?(.*)', fromMe: false, desc: DESC_RAND }, (async (message, match) => {
+Bixby.addCommand({ pattern: 'qrcode ?(.*)', fromMe: false, desc: DESC_RAND }, (async (message, match) => {
 if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORD, MessageType.text);
 var uria = encodeURI(match[1])
 var ttinullimageh = await axios.get(`https://api.zeks.me/api/qrencode?apikey=apivinz&text=${uria}`, { responseType: 'arraybuffer' })
 await message.client.sendMessage(message.jid,Buffer.from(ttinullimageh.data), MessageType.image)
 }));
     
-Asena.addCommand({ pattern: 'nulis ?(.*)', fromMe: false, desc: DESC_RAND }, (async (message, match) => {
+Bixby.addCommand({ pattern: 'nulis ?(.*)', fromMe: false, desc: DESC_RAND }, (async (message, match) => {
 if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORD, MessageType.text);
 var uria = encodeURI(match[1])
 var ttinullimageh = await axios.get(`https://api.zeks.me/api/nulis?apikey=apivinz&text=${uria}`, { responseType: 'arraybuffer' })
 await message.client.sendMessage(message.jid,Buffer.from(ttinullimageh.data), MessageType.image)
 }));
     
-Asena.addCommand({ pattern: 'sand ?(.*)', fromMe: false, desc: DESC_RAND }, (async (message, match) => {
+Bixby.addCommand({ pattern: 'sand ?(.*)', fromMe: false, desc: DESC_RAND }, (async (message, match) => {
 if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORD, MessageType.text);
 var uria = encodeURI(match[1])
 var ttinullimageh = await axios.get(`https://api.zeks.me/api/sandw?apikey=apivinz&text=${uria}`, { responseType: 'arraybuffer' })

deb31909f87865674ff846567d42bafdba5119dd
Ripper Sir <92292759+RIPPER-SER@users.noreply.github.com>
2022-03-28 15:03:01 +0530

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *Miss Helena*:  ```" + msg + "```"
+    return "✅ *Bixby Mowl*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *Miss Helena*:  ```" + msg + "```"
+    return "🛑 *Bixby Mowl*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *Miss Helena*:  ```" + msg + "```"
+    return "⏺️ *Bixby Mowl*:  ```" + msg + "```"
 }
 
 

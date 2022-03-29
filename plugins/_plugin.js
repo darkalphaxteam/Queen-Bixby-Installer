591fc6fecfebb636933f40830f6092712e86afe7
MIDU-SER <98685114+MIDU-SER@users.noreply.github.com>
2022-03-29 10:06:28 +0530

    Update _plugin.js

--- plugins/_plugin.js
+++ plugins/_plugin.js
@@ -144,7 +144,7 @@ Asena.addCommand({pattern: 'remove(?: |$)(.*)', fromMe: true, dontAddCommandList
         } else {
             await plugin[0].destroy();
             delete require.cache[require.resolve('./' + match[1] + '.js')]
-            fs.unlinkSync('./plugins/' + match[1] + '.js');
+            fs.unlinkSync('/root/Bixby/plugins/' + match[1] + '.js');
             await message.client.sendMessage(message.jid, Lang.DELETED, MessageType.text);        
             await new Promise(r => setTimeout(r, 1000));  
             await message.sendMessage(NLang.AFTER_UPDATE);

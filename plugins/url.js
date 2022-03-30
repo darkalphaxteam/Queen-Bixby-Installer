cf17c2e9b1cd9ac06146091edab03442aee7f5af
Ripper Sir <92292759+RIPPER-SER@users.noreply.github.com>
2022-03-30 11:13:31 +0530

    Update url.js

--- plugins/url.js
+++ plugins/url.js
@@ -1,9 +1,9 @@
-const skl = require('../events');
+const Bixby = require('../events');
 const {MessageType} = require('@adiwajshing/baileys');
 const Config = require('../config');
 
-let sk = Config.WORKTYPE == 'public' ? false : true
-skl.addCommand({pattern: 'url ?(.*)', fromMe: sk}, async (message, match) => {
+let ziyan = Config.WORKTYPE == 'public' ? false : true
+Bixby.addCommand({pattern: 'url ?(.*)', fromMe: ziyan}, async (message, match) => {
 
     await message.sendMessage('Use this site to get URL 👇:\n\n https://souravkl11.github.io/image-uploader/')
 

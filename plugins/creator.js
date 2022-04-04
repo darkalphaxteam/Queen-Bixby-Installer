e946f6ac7ab01b45eb91bba66e6a4a189df8f2a3
Ripper Sir <92292759+RIPPER-SER@users.noreply.github.com>
2022-04-05 00:05:35 +0530

    Adde image and Verified tick for contact ✅

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,17 +1,24 @@
+/* Copyright (C) 2022 RIPPER-SER.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+https://github.com/RIPPER-SER/bixbymowl
+*/
+
 const Bixby = require('../events');
-const {MessageType} = require('@adiwajshing/baileys');
+const {MessageType, Mimetype} = require('@adiwajshing/baileys');
 const exec = require('child_process').exec;
 const os = require("os");
 const fs = require('fs');
 const Config = require('../config')
-Bixby.addCommand({ pattern: 'creator ?(.*)', fromMe: false, desc: 'creater number' }, (async (message, match) => {
+Bixby.addCommand({ pattern: 'bosu ?(.*)', fromMe: false, desc: 'creator number' }, (async (message, match) => {
 
-//coded by saidali
+//coded by Ziyan
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:ZIYAN [OWNER]\n' // full name
-            + 'ORG:ZIYAN;\n' // 
+            + 'FN:ZIYAN [CREATOR]\n' // full name
+            + 'ORG: Creator Of Bixby Mowl;\n' // 
             + 'TEL;type=CELL;type=VOICE;waid=919446072492:+91 9446072492\n' // WhatsApp ID + phone number
             + 'END:VCARD'
-await message.client.sendMessage(message.jid,{displayname: "ZIYAN [OWNER]", vcard: vcard}, MessageType.contact)
+await message.client.sendMessage(message.jid,{displayname: "ZIYAN [OWNER]", vcard: vcard}, MessageType.contact, { mimetype: Mimetype.contact, quoted: message.data, ptt: true,quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(message.jid ? { remoteJid: "status@broadcast" } : {}) }, message: { orderMessage: { itemCount: 99999, status: 200, thumbnail: fs.readFileSync('./media/photo/bixby3.png'), surface: 200, message: Config.BOTSK, orderTitle: Config.BOTSK, "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg", "caption": Config.BOTSK + '\n', "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1080, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": fs.readFileSync('./media/photo/bixby3.png')}}}});
 }))
+

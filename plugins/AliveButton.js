e83772b9f68436c528a02b72d4f90bfdd9f3088b
Ripper Sir <92292759+RIPPER-SER@users.noreply.github.com>
2022-04-07 17:37:22 +0530

    Fixed ✅

--- plugins/AliveButton.js
+++ plugins/AliveButton.js
@@ -11,10 +11,10 @@ var ggg = Buffer.from(clh.cd, 'base64')
 var ddd = ggg.toString('utf-8')
 
 
-let whb = Config.WORKTYPE == 'public' ? false : true
+let ziyan = Config.WORKTYPE == 'public' ? false : true
 
-Amalser.addCommand({pattern: 'alive', fromMe: whb, dontAddCommandList: true}, (async (message, match) => {
-// send a buttons message!credit Amal
+Amalser.addCommand({pattern: 'alive', fromMe: ziyan, dontAddCommandList: true}, (async (message, match) => {
+// send a buttons message!credit Amalser nenba uyir only credits for nenban
     const buttons = [
         {buttonId: 'id1', buttonText: {displayText: Config.BIXBY_ALIVE }, type: 1},
         {buttonId: 'id2', buttonText: {displayText: Config.ALIVE_TWO }, type: 1}

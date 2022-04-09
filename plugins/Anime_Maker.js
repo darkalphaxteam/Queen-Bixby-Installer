ad4689f35b37fac65a3979d4746c89266f95ccea
Ripper Sir <92292759+RIPPER-SER@users.noreply.github.com>
2022-04-09 18:14:11 +0530

    Update Anime_Maker.js

--- plugins/Anime_Maker.js
+++ plugins/Anime_Maker.js
@@ -4,7 +4,7 @@ you may not use this file except in compliance with the License.
 https://github.com/RIPPER-SER/bixbymowl
 */
 //THIS PLUGIN BY ZIYAN
-//DONT COPY THIS WITHOUT CREDI
+//DONT COPY THIS WITHOUT CREDIT
 
 
 //BASIC REQUIREMENTS

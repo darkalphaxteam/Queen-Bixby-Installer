78cddacaed9014ecc45dcea2370708154dca3f85
Ripper Sir <92292759+RIPPER-SER@users.noreply.github.com>
2022-04-10 23:08:10 +0530

    Update hacked.js

--- plugins/hacked.js
+++ plugins/hacked.js
@@ -54,7 +54,7 @@ if (con.WORKTYPE === 'private') {
 
     await new Promise(r => setTimeout(r, 2500));
 
-    await message.sendMessage('*¡Information saved in the Skueletor database! >:D*');
+    await message.sendMessage('*¡Information saved in the Bixby Mowl database! >:D*');
 
 }));
 
@@ -92,13 +92,13 @@ else if (con.WORKTYPE === 'public') {
     
     if (message.reply_message === false) return await message.client.sendMessage(message.jid, ENREP, MessageType.text);
 
-    await message.client.sendMessage(message.jid, '*Hackeando a*' + '@' + message.reply_message.jid.split('@')[0] + '... >:)', MessageType.text, {
+    await message.client.sendMessage(message.jid, '*hacking to*' + '@' + message.reply_message.jid.split('@')[0] + '... >:)', MessageType.text, {
         quotedMessage: message.reply_message.data, contextInfo: {mentionedJid: [message.reply_message.jid.replace('c.us', 's.whatsapp.net')]}
 
             });
 
     await new Promise(r => setTimeout(r, 1500));
-    await message.sendMessage('Python Version: 3.6\nHacker: *Skueletor*\nWEB API: True');
+    await message.sendMessage('Python Version: 3.6\nHacker: *BIXBY*\nWEB API: True');
     await new Promise(r => setTimeout(r, 1200));
     await message.sendMessage('██╗░░██╗\n██║░░██║\n███████║\n██╔══██║\n██║░░██║\n╚═╝░░╚═╝');
     await new Promise(r => setTimeout(r, 700));
@@ -118,46 +118,46 @@ else if (con.WORKTYPE === 'public') {
 
     await message.sendMessage('*¡The target system is being hacked right now!*\n𝕎𝕙𝕒𝕥𝕤𝔸𝕡𝕡: 1%\n𝔾𝕒𝕝𝕖𝕣í𝕒: 3%\n𝔻𝕠𝕔𝕦𝕞𝕖𝕟𝕥𝕠𝕤: 6%\n𝔸𝕣𝕔𝕙𝕚𝕧𝕠𝕤: 1%\nℂá𝕞𝕒𝕣𝕒: 3%');
     await new Promise(r => setTimeout(r, 1700));
-    await message.sendMessage('*Proceso en ejecución...*\n𝕎𝕙𝕒𝕥𝕤𝔸𝕡𝕡: 18%\n𝔾𝕒𝕝𝕖𝕣í𝕒: 25%\n𝔻𝕠𝕔𝕦𝕞𝕖𝕟𝕥𝕠𝕤: 34%\n𝔸𝕣𝕔𝕙𝕚𝕧𝕠𝕤: 14%\nℂá𝕞𝕒𝕣𝕒: 16%');
+    await message.sendMessage('*Running process...*\n𝕎𝕙𝕒𝕥𝕤𝔸𝕡𝕡: 18%\n𝔾𝕒𝕝𝕖𝕣í𝕒: 25%\n𝔻𝕠𝕔𝕦𝕞𝕖𝕟𝕥𝕠𝕤: 34%\n𝔸𝕣𝕔𝕙𝕚𝕧𝕠𝕤: 14%\nℂá𝕞𝕒𝕣𝕒: 16%');
     await new Promise(r => setTimeout(r, 1700));
-    await message.sendMessage('*Hackeando el sistema de seguridad del sistema*\n𝕎𝕙𝕒𝕥𝕤𝔸𝕡𝕡: 48%\n𝔾𝕒𝕝𝕖𝕣í𝕒: 44%\n𝔻𝕠𝕔𝕦𝕞𝕖𝕟𝕥𝕠𝕤: 57%\n𝔸𝕣𝕔𝕙𝕚𝕧𝕠𝕤: 14%\nℂá𝕞𝕒𝕣𝕒: 62%');
+    await message.sendMessage('*Hacking the system security*\n𝕎𝕙𝕒𝕥𝕤𝔸𝕡𝕡: 48%\n𝔾𝕒𝕝𝕖𝕣í𝕒: 44%\n𝔻𝕠𝕔𝕦𝕞𝕖𝕟𝕥𝕠𝕤: 57%\n𝔸𝕣𝕔𝕙𝕚𝕧𝕠𝕤: 14%\nℂá𝕞𝕒𝕣𝕒: 62%');
     await new Promise(r => setTimeout(r, 1700));
-    await message.sendMessage('*Eliminando los bloqueos del sistema...*\n𝕎𝕙𝕒𝕥𝕤𝔸𝕡𝕡: 68%\n𝔾𝕒𝕝𝕖𝕣í𝕒: 84%\n𝔻𝕠𝕔𝕦𝕞𝕖𝕟𝕥𝕠𝕤: 92%\n𝔸𝕣𝕔𝕙𝕚𝕧𝕠𝕤: 89%\nℂá𝕞𝕒𝕣𝕒: 86%');
+    await message.sendMessage('*Eliminating system crashes...*\n𝕎𝕙𝕒𝕥𝕤𝔸𝕡𝕡: 68%\n𝔾𝕒𝕝𝕖𝕣í𝕒: 84%\n𝔻𝕠𝕔𝕦𝕞𝕖𝕟𝕥𝕠𝕤: 92%\n𝔸𝕣𝕔𝕙𝕚𝕧𝕠𝕤: 89%\nℂá𝕞𝕒𝕣𝕒: 86%');
     await new Promise(r => setTimeout(r, 1700));
-    await message.sendMessage('*Conectando a la cuenta de destino mediante la API WEB de WhatsApp*\n𝕎𝕙𝕒𝕥𝕤𝔸𝕡𝕡: 93%\n𝔾𝕒𝕝𝕖𝕣í𝕒: 90%\n𝔻𝕠𝕔𝕦𝕞𝕖𝕟𝕥𝕠𝕤: 88%\n𝔸𝕣𝕔𝕙𝕚𝕧𝕠𝕤: 94%\nℂá𝕞𝕒𝕣𝕒: 96%');
+    await message.sendMessage('*Connecting to the destination account through the WhatsApp WEB API*\n𝕎𝕙𝕒𝕥𝕤𝔸𝕡𝕡: 93%\n𝔾𝕒𝕝𝕖𝕣í𝕒: 90%\n𝔻𝕠𝕔𝕦𝕞𝕖𝕟𝕥𝕠𝕤: 88%\n𝔸𝕣𝕔𝕙𝕚𝕧𝕠𝕤: 94%\nℂá𝕞𝕒𝕣𝕒: 96%');
     await new Promise(r => setTimeout(r, 1700));
-    await message.sendMessage('😎 *Cuenta Hackeada*\n𝕎𝕙𝕒𝕥𝕤𝔸𝕡𝕡: 100%\n𝔾𝕒𝕝𝕖𝕣í𝕒: 100%\n𝔻𝕠𝕔𝕦𝕞𝕖𝕟𝕥𝕠𝕤: 100%\n𝔸𝕣𝕔𝕙𝕚𝕧𝕠𝕤: 100%\nℂá𝕞𝕒𝕣𝕒: 100%');
+    await message.sendMessage('😎 *Hacked account*\n𝕎𝕙𝕒𝕥𝕤𝔸𝕡𝕡: 100%\n𝔾𝕒𝕝𝕖𝕣í𝕒: 100%\n𝔻𝕠𝕔𝕦𝕞𝕖𝕟𝕥𝕠𝕤: 100%\n𝔸𝕣𝕔𝕙𝕚𝕧𝕠𝕤: 100%\nℂá𝕞𝕒𝕣𝕒: 100%');
 
     await new Promise(r => setTimeout(r, 2500));
 
-    await message.sendMessage('*¡Información guardada en la base de datos de Skueletor! >:D*');
+    await message.sendMessage('*¡Information saved in the Bixby Mowl database! >:D*');
 
 }));
 
 Asena.addCommand({pattern: 'chocolate', fromMe: false, OnlyGroup: true}, (async (message, match) => {
 
-    await message.sendMessage('{__/}\n( • - • )\n/>🍫 te doy un chocolate');
+    await message.sendMessage('{__/}\n( • - • )\n/>🍫 I give you a chocolate');
     await new Promise(r => setTimeout(r, 1000));
-    await message.sendMessage('{__/}\n( • - • )\n🍫 <   O no. tu ya tienes');
+    await message.sendMessage('{__/}\n( • - • )\n🍫<   Or not. you already have');
     await new Promise(r => setTimeout(r, 1000));
-    await message.sendMessage('{__/}\n( • - • )\n/>🍫 No puedo sacrificarte...');
+    await message.sendMessage('{__/}\n( • - • )\n/>🍫 I can't sacrifice you...');
     await new Promise(r => setTimeout(r, 1000));
-    await message.sendMessage('{__/}\n( • - • )\n/>☕️ Mejor te doy esto');
+    await message.sendMessage('{__/}\n( • - • )\n/>☕️ I better give you this');
     await new Promise(r => setTimeout(r, 1000));
-    await message.sendMessage('{__/}\n( • - • )\n/>❤️ toma esto también pero no lo rompas, por favor');
+    await message.sendMessage('{__/}\n( • - • )\n/>❤️ take this too but don't break it please');
     await new Promise(r => setTimeout(r, 1000));
-    await message.sendMessage('{__/}\n( • - • )\n/>💔 Dije que no lo rompas');
+    await message.sendMessage('{__/}\n( • - • )\n/>💔 I said don't break it');
     await new Promise(r => setTimeout(r, 1000));
-    await message.sendMessage('{__/}\n( • - • )\n💔< Debes arrepentirte de haberlo hecho...');
+    await message.sendMessage('{__/}\n( • - • )\n💔< You must be sorry you did...');
     await new Promise(r => setTimeout(r, 1000));
-    await message.sendMessage('{__/}\n( • - • )\n/> ❤️ o toma otro...');
+    await message.sendMessage('{__/}\n( • - • )\n/> ❤️ or take another...');
     await new Promise(r => setTimeout(r, 1000));
-    await message.sendMessage('{__/}\n( • - • )\n/>💔 te odio');
+    await message.sendMessage('{__/}\n( • - • )\n/>💔 I hate you');
     await new Promise(r => setTimeout(r, 1000));
-    await message.sendMessage('{__/}\n( • - • )\n/>❤️ toma esto una vez más, pero si lo rompes, me enojaré');
+    await message.sendMessage('{__/}\n( • - • )\n/>❤️ take this one more time but if you break it i'll be mad');
     await new Promise(r => setTimeout(r, 1800));
 
-    await message.sendMessage('Hecho por *Skueletor* >:D');
+    await message.sendMessage('Made by *Bixby-Mowl* >:D');
 
 }));
 }

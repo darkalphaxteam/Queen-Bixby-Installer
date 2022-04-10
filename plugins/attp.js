94d265cab66dd5b003f7b69e59e27d1aceb40ff5
Ripper Sir <92292759+RIPPER-SER@users.noreply.github.com>
2022-04-10 16:48:28 +0530

    Update attp.js

--- plugins/attp.js
+++ plugins/attp.js
@@ -4,7 +4,7 @@ you may not use this file except in compliance with the License.
 https://github.com/RIPPER-SER/bixbymowl
 */
 
-THIS PlUGIN BY WHATS ASENA REDITED ZIYAN
+//THIS PlUGIN BY WHATS ASENA REDITED ZIYAN
 
 
 //BASIC REQUIREMENTS

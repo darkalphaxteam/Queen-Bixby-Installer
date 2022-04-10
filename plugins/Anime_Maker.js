39bd910291f72a1623d0ba55fef41a0fe7f2ced0
Ripper Sir <92292759+RIPPER-SER@users.noreply.github.com>
2022-04-10 12:02:52 +0530

    FIX✅

--- plugins/Anime_Maker.js
+++ plugins/Anime_Maker.js
@@ -197,6 +197,8 @@ Bixby.addCommand({ pattern: 'anime16 ?(.*)', fromMe: false, dontAddCommandList: 
 
     }));
 
+ }
+
 if (Config.WORKTYPE == 'private') {
 Bixby.addCommand({pattern: 'animepack', fromMe: true, desc: Ln,}, (async (message, match) => {
 

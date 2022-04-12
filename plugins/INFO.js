2faf4de034c97f4a154626241809f8b850a04bcf
Ripper Sir <92292759+RIPPER-SER@users.noreply.github.com>
2022-04-12 06:33:42 +0530

    Update INFO.js

--- plugins/INFO.js
+++ plugins/INFO.js
@@ -5,10 +5,10 @@ const axios = require('axios');
 const Language = require('../language');
 const Lang = Language.getString('wallpaper');
 
-Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the bot"}, (async (message, match) => {
+Asena.addCommand({pattern: 'infobot', fromMe: false, desc: "Gives github link of the bot"}, (async (message, match) => {
 
     var skl = await axios.get("https://i.imgur.com/w89FHm7.jpeg", { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: ``}) 
+    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `Hey i am ziyan`}) 
 
 }));

0d5a87af474e2e20a447d046485231535b42b1d6
King Dark Alpha <93303850+thinura2003@users.noreply.github.com>
2022-04-26 19:42:49 +0530

    Wait for update

--- plugins/scrapers.js
+++ plugins/scrapers.js
@@ -1035,7 +1035,7 @@ else if (config.WORKTYPE == 'public') {
                 writer.addTag();
 
                 
-                await message.client.sendMessage(message.jid,Buffer.from(writer.arrayBuffer), MessageType.document, {filename: 'for iphone' + '.mp3', mimetype: 'audio/mpeg', quoted: message.data});
+                await message.client.sendMessage(message.jid,Buffer.from(writer.arrayBuffer), MessageType.document, {filename: 'Queen Bixby 2022' + '.mp3', mimetype: 'audio/mpeg', quoted: message.data});
             });
     }));
 
